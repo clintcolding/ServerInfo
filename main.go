d31c63e59b8666233d7fb8626784c80d02bbb13c
Clint Colding <clintcolding@gmail.com>
2019-08-28 21:40:06 -0400

    Added local ip

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"runtime"
 )
+
+func GetIP() net.IP {
+	conn, _ := net.Dial("udp", "1.2.3.4:80")
+	conn.Close()
+	ip := conn.LocalAddr().(*net.UDPAddr)
+	return ip.IP
+}
 
 func main() {
 	// Collect memory stats
@@ -19,6 +27,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Print collected info with newline
 		fmt.Fprintf(w, "Hostname: %s\n", h)
+		fmt.Fprintf(w, "IP: %s\n", GetIP())
 		fmt.Fprintf(w, "Platform: %s\n", runtime.GOOS)
 		fmt.Fprintf(w, "CPU: %d\n", runtime.NumCPU())
 		fmt.Fprintf(w, "RAM: %d", m.Sys)
